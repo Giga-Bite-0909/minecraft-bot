2cd6c6e82a0e968dcd060ca82821956974007b8a
Giga-Bite-0909 <legendofminecraft9999@gmail.com>
2026-03-15 09:36:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const mineflayer = require('mineflayer')
 
-function createBot() {
+function createBot(){
 
 const bot = mineflayer.createBot({
   host: 'FaaTheDawg.aternos.me',
@@ -10,16 +10,67 @@ const bot = mineflayer.createBot({
 })
 
 bot.on('spawn', () => {
+
 console.log("Bot joined")
 
+// random chat
+const messages = [
+"hello",
+"anyone here?",
+"nice server",
+"just chilling",
+"what's up"
+]
+
 setInterval(()=>{
-bot.chat("hello")
-},180000)
+const msg = messages[Math.floor(Math.random()*messages.length)]
+bot.chat(msg)
+},60000)
+
+
+// random movement
+setInterval(()=>{
+
+const actions = ['forward','back','left','right']
+const action = actions[Math.floor(Math.random()*actions.length)]
+
+bot.setControlState(action,true)
+
+setTimeout(()=>{
+bot.setControlState(action,false)
+},2000)
+
+},15000)
+
+
+// random jumping
+setInterval(()=>{
+bot.setControlState('jump',true)
+
+setTimeout(()=>{
+bot.setControlState('jump',false)
+},500)
+
+},20000)
+
+
+// random looking
+setInterval(()=>{
+const yaw = Math.random() * Math.PI * 2
+const pitch = (Math.random() - 0.5) * Math.PI
+bot.look(yaw,pitch)
+},10000)
 
 })
 
+
 bot.on('end',()=>{
+console.log("Reconnecting...")
 setTimeout(createBot,5000)
+})
+
+bot.on('error',(err)=>{
+console.log(err)
 })
 
 }
